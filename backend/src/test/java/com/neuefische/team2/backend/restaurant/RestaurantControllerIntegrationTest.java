146e8a83229cd2553910a0bb1abe1e5fada74ff3
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-02 10:37:55 +0200

    Add RestaurantControllerIntegrationTest class
    This commit adds the integration test class for the RestaurantController. The added class includes one test that checks whether an empty list is returned when querying for all restaurants. A placeholder for a future test, concerning the POST endpoint, has also been added.

---
+++ backend/src/test/java/com/neuefische/team2/backend/restaurant/RestaurantControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.neuefische.team2.backend.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RestaurantControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllProducts_whenProductInDB_thenReturnEmptyList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/restaurants"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("[]"));
+    }
+
+    // TODO: Write a test to receive one restaurant as soon as POST endpoint is implemented.
+}
