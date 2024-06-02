0de040f9a02597b83a60fa3be5a8e1543dd2b370
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-02 10:35:14 +0200

    Add RestaurantServiceTest
    Created a new file, RestaurantServiceTest.java, that includes tests for the RestaurantService class. The tests verify if the 'getRestaurants' method correctly returns an empty list when there are no restaurants in the DB and a list of one restaurant when there is one in the DB.

---
+++ backend/src/test/java/com/neuefische/team2/backend/restaurant/RestaurantServiceTest.java
@@ -0,0 +1,45 @@
+package com.neuefische.team2.backend.restaurant;
+
+import com.neuefische.team2.backend.restaurant.domain.Restaurant;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class RestaurantServiceTest {
+
+    private final RestaurantRepository mockRestaurantRepository = mock(RestaurantRepository.class);
+    private final RestaurantService restaurantService = new RestaurantService(mockRestaurantRepository);
+
+    @Test
+    void getRestaurants_whenNoRestaurantsInDB_thenReturnEmptyList() {
+        //GIVEN
+        when(mockRestaurantRepository.findAll()).thenReturn(Collections.emptyList());
+
+        // WHEN
+        List<Restaurant> actual = restaurantService.getRestaurants();
+
+        //THEN
+        verify(mockRestaurantRepository).findAll();
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    void getRestaurants_whenOneRestaurantsInDB_thenReturnListOfOne() {
+        //GIVEN
+        Restaurant restaurant = new Restaurant("1", "The Mockingbird", "New York");
+        when(mockRestaurantRepository.findAll()).thenReturn(Collections.singletonList(restaurant));
+
+        // WHEN
+        List<Restaurant> actual = restaurantService.getRestaurants();
+
+        //THEN
+        verify(mockRestaurantRepository).findAll();
+        List<Restaurant> expected = List.of(restaurant);
+        assertEquals(expected, actual);
+    }
+}
