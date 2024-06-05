dbf159254b9a12c23d705c1fbf74c357293cd283
daniel-pohl <danielpohl@mail.de>
2024-06-05 11:42:30 +0200

    fixed stuff of PR next change

--- backend/src/test/java/com/neuefische/team2/backend/restaurant/RestaurantControllerIntegrationTest.java
+++ backend/src/test/java/com/neuefische/team2/backend/restaurant/RestaurantControllerIntegrationTest.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
